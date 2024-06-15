54027b713b84e39bc4fa2ea2eaa786070d4cc7fb
Cesar Infante <a22310251@live.ceti.mx>
2024-06-15 03:11:03 -0600

    Version 3

--- src/main.cpp
+++ src/main.cpp
@@ -5,47 +5,25 @@
 #include <string>
 #include <fstream>
 #include <Dibujo.hpp>
-#include <chrono>
-#include <conio.h> // Para la función _kbhit() y _getch() en Windows
 
 using namespace std;
 using namespace ftxui;
 
 int main (int argc, char const *argv[])
 {
-    // Inicializar objeto Dibujo con la ruta al archivo de texto.
     Dibujo snake("./assets/images/snake.txt");
 
-    int fotograma = 0;
-    bool running = true;
-
-    while(running)
+    int fotograma=0;
+    while(true)
     {
-        // Dormir el hilo principal por 100 milisegundos.
-        this_thread::sleep_for(chrono::milliseconds(100));
+        this_thread::sleep_for(0.1s);
         fotograma++;
-
-        // Crear el elemento del spinner.
-        Element personaje = spinner(21, fotograma) | bold | color(Color::Purple) | bgcolor(Color::White);
-        
-        // Crear el elemento del dibujo.
+        Element personaje = spinner (21,fotograma)| bold | color(Color::Purple) | bgcolor(Color::White);
         Element dibujo = hbox({personaje, snake.GetElement()});
-        
-        // Crear y renderizar la pantalla.
-        Screen pantalla = Screen::Create(Dimension::Full());
-        Render(pantalla, dibujo);
+        Screen pantalla= Screen::Create(Dimension::Full());
+        Render(pantalla,dibujo);
         pantalla.Print();
-        cout << pantalla.ResetPosition();
-
-        // Salir del bucle si se presiona una tecla.
-        if (_kbhit())
-        {
-            char ch = _getch();
-            if (ch == 'q' || ch == 'Q')  // Presionar 'q' o 'Q' para salir
-            {
-                running = false;
-            }
-        }
+        cout<<pantalla.ResetPosition();
     }
     
     return EXIT_SUCCESS;
